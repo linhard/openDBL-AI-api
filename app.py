821e18ae6877d474079aa7af6b6b95624e99ecfe
linhard <linhard@iabi.eu>
2025-03-26 11:11:48 +0100

    Update app.py
    Typo

--- app.py
+++ app.py
@@ -43,4 +43,7 @@ def get_ontology(id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
